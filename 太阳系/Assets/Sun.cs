937851cfae03dd35325300cf74c1f3956b8cdecd
zxy0616 <hantangs@outlook.com>
2020-10-05 21:24:36 +0800

    sun

---
+++ 太阳系/Assets/Sun.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class Sun : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        GameObject.Find("Earth").transform.RotateAround(Vector3.zero, new Vector3(0, 1, 0), 30 * Time.deltaTime);
+
+        GameObject.Find("Earth").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Mercury").transform.RotateAround(Vector3.zero, new Vector3(1, 1, 0), 25 * Time.deltaTime);
+
+        GameObject.Find("Mercury").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Venus").transform.RotateAround(Vector3.zero, new Vector3(0, 1, 1), 20 * Time.deltaTime);
+
+        GameObject.Find("Venus").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Mars").transform.RotateAround(Vector3.zero, new Vector3(2, 1, 0), 45 * Time.deltaTime);
+
+        GameObject.Find("Mars").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Jupiter").transform.RotateAround(Vector3.zero, new Vector3(1, 2, 0), 35 * Time.deltaTime);
+
+        GameObject.Find("Jupiter").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Saturn").transform.RotateAround(Vector3.zero, new Vector3(0, 1, 2), 40 * Time.deltaTime);
+
+        GameObject.Find("Saturn").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Uranus").transform.RotateAround(Vector3.zero, new Vector3(0, 2, 1), 45 * Time.deltaTime);
+
+        GameObject.Find("Uranus").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+        GameObject.Find("Neptune").transform.RotateAround(Vector3.zero, new Vector3(1, 1, 1), 50 * Time.deltaTime);
+
+        GameObject.Find("Neptune").transform.Rotate(Vector3.up * Time.deltaTime * 10000);
+
+    }
+
+}

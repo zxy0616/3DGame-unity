63f63314e31f32630f6b3bce208fff56fbbbe8e6
zxy0616 <hantangs@outlook.com>
2020-11-12 00:50:57 +0800

    hw6

---
+++ Hit_UFO - Adapter/Assets/Scripts/Interface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SSActionEventType : int { Started, Competeted }
+
+public interface ISSActionCallback
+{
+    void SSActionEvent(SSAction source, SSActionEventType events = SSActionEventType.Competeted,
+        int intParam = 0, string strParam = null, Object objectParam = null);
+}
+//场景控制类
+public interface ISceneController
+{
+    void LoadResources();
+}
+//在已有的接口类中添加击中方法，配置为Disk的Adapter接口
+public interface IUserAction
+{
+    void Restart();
+    void Hit(Vector3 pos);
+    void GameOver();
+    int GetScore();
+    void BeginGame();
+}
---
+++ Hit_UFO - Adapter/Assets/Scripts/UFOFlyAction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOFlyAction : SSAction {
+    public float gravity = -5;
+    //设置物体重力
+    private Vector3 startVector;//物体随机出现
+    private Vector3 gravityVector = Vector3.zero;//物体做平抛运动时改变的位置
+    private float time;//记录物体运动时间
+    private Vector3 currentAngle = Vector3.zero;//记录抛出角度
+
+    private UFOFlyAction()
+    {
+
+    }
+
+    public static UFOFlyAction GetSSAction(Vector3 direction, float angle, float power)
+    {
+        UFOFlyAction action = CreateInstance<UFOFlyAction>();
+        if (direction.x == -1)
+        {
+            action.startVector = Quaternion.Euler(new Vector3(0, 0, -angle)) * Vector3.left * power;
+        }
+        else
+        {
+            action.startVector = Quaternion.Euler(new Vector3(0, 0, angle)) * Vector3.right * power;
+        }
+        return action;
+    }
+
+    public override void Update()
+    {
+        time += Time.fixedDeltaTime;
+        gravityVector.y = gravity * time;
+
+        transform.position += (startVector + gravityVector) * Time.fixedDeltaTime;
+        currentAngle.z = Mathf.Atan((startVector.y + gravityVector.y) / startVector.x);
+        transform.eulerAngles = currentAngle;
+
+        if(this.transform.position.y < -10)
+        {
+            this.destroy = true;
+            this.callback.SSActionEvent(this);
+        }
+    }
+
+    public override void Start()
+    {
+
+    }
+}
